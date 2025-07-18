d649da015cec46e144fe67655b090b67e2cfb907
andrewisokoe <andrewisoko1998@gmail.com>
2025-07-18 17:21:54 +0100

    Styling input button elements.

--- react-app/src/components/TwoGrids.tsx
+++ react-app/src/components/TwoGrids.tsx
@@ -1,4 +1,6 @@
 import '../css/TwoGrids.css'
+import guessWhosThat from '../assets/Guess the man.png'
+import searchButton from '../assets/Button.png'
 
 
 const TwoGrids = () => {
@@ -10,8 +12,9 @@ const TwoGrids = () => {
                 @import url('https://fonts.cdnfonts.com/css/battle-tough');
             </style>
             <h2 style={{
+              marginTop: 40,
               marginBottom:40,
-              marginLeft: 30,
+              textAlign: 'center',
               
 
             }}>Aliens Races</h2>
@@ -24,9 +27,25 @@ const TwoGrids = () => {
           </ol>
         
         <div>
-            <div>Black box</div>
-            <input type="text" placeholder="Guess the alien race" />
-            <button></button>
+            <div 
+            style={{
+              marginTop:60,
+              marginBottom:15,
+              marginLeft: 80,
+              
+            }}
+            className='card'>
+              <img src={guessWhosThat} alt="Card Image" className="card-img"></img>
+            </div>
+       
+            <div 
+              className='wrapper'
+              style={{
+                marginLeft:85,
+              }}>
+              <input type="text" placeholder="Guess the alien race"/>
+              <button> click </button>
+            </div>
         </div>
 
     </div>

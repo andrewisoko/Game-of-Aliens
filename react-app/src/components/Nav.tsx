d05f181579aac41b08b23c584d7a29b6c14bb45c
andrewisokoe <andrewisoko1998@gmail.com>
2025-07-25 12:39:03 +0100

    Responsive burger button.

--- react-app/src/components/Nav.tsx
+++ react-app/src/components/Nav.tsx
@@ -1,19 +1,66 @@
 import '../css/Nav.css'
+import React, { useState } from "react";
 
 function Nav(){
 
+    const [burgerClass,setBurgerClass] = useState("burger-bar unclicked")
+    const [menuClass,setMenuClass] = useState("menu hidden")
+    const [burgerClicked,setBurgerClicked] = useState(false)
+
+
+    const boxPopup = () =>{
+        if(!burgerClicked){
+            setBurgerClicked(true)
+        }
+        else{
+           setBurgerClicked(false) 
+        }
+    }
+
+
     return <nav className="navbar" style={{marginTop:10}}>
+
         <div className='text-display'> 
-            <p>Facebook</p>
-            <p>Tiktok</p>
-            <p>Whatsapp</p>
-            <p>Telegram</p>
+            <a href='https://www.facebook.com/'>Facebook</a>
+            <a href='https://www.tiktok.com/login/'>Tiktok</a>
+            <a href='https://web.whatsapp.com/#'>Whatsapp</a>
+            <a href='https://web.telegram.org/k/'>Telegram</a>
         </div>
-        <div className='burger-button'style={{ cursor:'pointer'}}>
-            <span ></span>
+        
+        <div 
+            className='burger-button'
+            onClick={boxPopup}
+            style={{ cursor:'pointer'}}>
             <span></span>
             <span></span>
+            <span></span>
         </div>
+
+        {burgerClicked && (
+            <div style={{
+                textAlign: 'center'
+            }}className = "menu">
+            <div style={{
+                color:'grey'
+            }}><strong>Facts on Aliens</strong></div>
+            <hr />
+             <a href="https://en.wikipedia.org/wiki/Fermi_paradox">The fermi paradox</a>
+            <hr />
+                <a href="https://www.bbc.co.uk/news/articles/c39jj9vkr34o">Strongest evidence of life outside earth</a>
+            <hr />
+                <a href="https://thereader.mitpress.mit.edu/history-speculation-about-aliens/">The first alien propagandist</a>
+            <hr />
+            <hr />
+                <a href="https://www.britannica.com/topic/unidentified-flying-object/Other-investigations-of-UFOs">UFO</a>
+            <hr />
+            <hr />
+                <a href="https://en.wikipedia.org/wiki/Zecharia_Sitchin">Anunaki</a>
+            <hr />
+            <div>👽</div>
+        </div>
+
+        )}
+
     </nav>
 }
 
